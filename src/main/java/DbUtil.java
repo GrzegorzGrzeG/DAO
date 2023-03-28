2d472c93481331e975bc252eccbd44c65eee5161
GrzegorzGrzeG <g.grzsgorczyk@gmail.com>
2023-03-28 11:08:59 +0200

    szkieet

---
+++ src/main/java/DbUtil.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+public class DbUtil {
+    private static final String DB_NAME = "";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+    public Connection connection(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
